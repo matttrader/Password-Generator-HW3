29722f8dec8c56ffe7f48c9a97d8fa5b87e2ac1d
Matt Trader <matthewtrader@sbcglobal.net>
2020-10-31 21:35:32 -0400

    Completed the Password-Generator project.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # homework-3
+The goal of this project was to create a Password Generator.  The password would be randomized with uppercase letters, lowercase letters, numbers, and special character, if desired.  The password created must be 8 or more characters in length.
+
+The purpose of this project was to practice and apply concepts learned after the third week of our Coding Bootcamp.
+
+This is an html file with a CSS stylesheet and JS.
+
+Below are a few select screenshots of the Password Generator.
+![Start of the Password Generator](C:\Users\Matt\Desktop\Coding-Projects\homework-3\assets/Password-Generator.PNG)
+
+![Completion of the Password Generator](C:\Users\Matt\Desktop\Coding-Projects\homework-3\assets/Completed-Password.PNG)
+
+Here is the link to "Matt's Highly Sophisticated Password Generator"
+file:///C:/Users/Matt/Desktop/Coding-Projects/homework-3/index.html 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,79 @@
-// Assignment Code
+
+var lowerString = "abcdefghijklmnopqrstuvwxyz";
+var lowerArray = lowerString.split("");
+console.log(lowerArray);
+
+var upperArray = lowerString.toUpperCase().split("");
+console.log(upperArray);
+
+var numericString = "0123456789";
+var numericArray = numericString.split("");
+console.log(numericString);
+
+var specialcharString = "!@#$%^&*";
+var specialcharArray = specialcharString.split("");
+console.log(specialcharString);
+
+
+function getOptions() {
+  var passLength = parseInt(prompt("How many characters do you want in your password? The minimum is 8 characters."));
+  if(passLength < 8) {
+    alert("invalid");
+    return;
+  }
+  var isLower = confirm("Do you want lower case letters in your password?");
+  var isUpper = confirm("Do you want upper case letters in your password?");
+  var isNumeric = confirm("Do you want numbers in your password?");
+  var isSpecialchar = confirm("Do you want special characters in your password?");
+
+  
+  var options = {
+    passLength,
+    isUpper,
+    isLower,
+    isNumeric,
+    isSpecialchar
+  }
+ 
+  return options
+}
+
+function generatePassword(){
+  var myOptions = getOptions();
+  var superArray = [];
+   var results = [];
+
+  if(myOptions.isLower === true) {
+    superArray = superArray.concat(lowerArray)
+    console.log(superArray)
+  }
+  if(myOptions.isUpper === true) {
+    superArray = superArray.concat(upperArray)
+    console.log(superArray)
+  }
+  if(myOptions.isNumeric === true) {
+    superArray = superArray.concat(numericArray)
+    console.log(superArray)
+  }
+  if(myOptions.isSpecialchar === true) {
+    superArray = superArray.concat(specialcharArray)
+    console.log(superArray)
+  }
+
+
+
+  for(var i = 0; i < myOptions.passLength; i++) {
+    var index = Math.floor(Math.random() * superArray.length);
+    var digit = superArray[index];
+    results.push(digit)
+    console.log(digit)
+  }
+  return results.join("");
+}
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,5 +82,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
